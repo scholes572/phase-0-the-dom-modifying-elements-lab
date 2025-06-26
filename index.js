4beece05dd9c2875fe73533f22d384c51901dcea
Agostino <agostinoscholes572@gmail.com>
2025-06-27 00:05:34 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main =  document.getElementById('main');
+if (main) {
+    main.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Scholes is the champion';
+document.body.appendChild(newHeader);
